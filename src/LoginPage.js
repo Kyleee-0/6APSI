68c9ba2910c914da973e4bffc268181b7d2974a2
Hakaii1 <150959650+Hakaii1@users.noreply.github.com>
2025-06-23 23:07:11 +0800

    Update LoginPage.js

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
 
 function LoginPage() {
- 
   return (
-    <div>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-       
-      /><br />
-      <input
-        type="password"
-        placeholder="Password"
-     
-      /><br />
-      <button >Login</button>
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh' 
+    }}>
+      <div style={{ 
+        padding: '20px', 
+        border: '5px solid #ccc', 
+        borderRadius: '5px', 
+        textAlign: 'center' 
+      }}>
+        <h2>Login</h2>
+        <input type="text" placeholder="Username" style={{ display: 'block', margin: '10px auto' }} /><br />
+        <input type="password" placeholder="Password" style={{ display: 'block', margin: '10px auto' }} /><br />
+        <button style={{ marginTop: '10px' }}>Login</button>
+      </div>
     </div>
   );
 }
